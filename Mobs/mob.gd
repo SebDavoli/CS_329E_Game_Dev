95fcbf0efed096354a0a41ef3fc7f2c96c2bee21
UTDAC <107150263+UTDAC@users.noreply.github.com>
2023-09-25 13:19:51 -0500

    update

--- Mobs/mob.gd
+++ Mobs/mob.gd
@@ -5,7 +5,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite2D.play("move")
+	$AnimatedSprite2D.play(0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
