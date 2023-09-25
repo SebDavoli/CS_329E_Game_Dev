489c43ba188567bef3aee748fd20a8f3550e1e4a
UTDAC <107150263+UTDAC@users.noreply.github.com>
2023-09-25 13:37:47 -0500

    sd

--- main.gd
+++ main.gd
@@ -4,8 +4,15 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$MobTimer.start()
+	print("penis!")
 
+
+
+func new_game():
+	score = 0
+	#$sola.start(250,250)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,7 +21,7 @@ func _process(delta):
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
-	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	var mob_spawn_location = (250.0,250.0) #get_node("MobPath/MobSpawnLocation")
 	mob_spawn_location.progress_ratio = randf()
 	var direction = mob_spawn_location.rotation + PI/2
 	mob.position = mob_spawn_location.position
