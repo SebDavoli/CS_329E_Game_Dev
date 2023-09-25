9fcff723b7aaee72f3844ee7c30144ac539e3c35
UTDAC <107150263+UTDAC@users.noreply.github.com>
2023-09-25 13:10:47 -0500

    integrating mob spawning into main scene

--- main.gd
+++ main.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	new_game()
 	pass # Replace with function body.
 
 
@@ -16,3 +17,10 @@ func _on_mob_timer_timeout():
 	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
 	mob_spawn_location.progress_ratio = randf()
 	var direction = mob_spawn_location.rotation + PI/2
+	mob.position = mob_spawn_location.position
+	direction += randf_range(-PI/4,PI/4)
+	mob.rotation = direction
+	var velocity = Vector2(randf_range(150.0,250.0),0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
