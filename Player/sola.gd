7fb6e443a175e64c13ad9793a7661ee472df99e9
rachelmc2020 <118246000+rachelmc2020@users.noreply.github.com>
2023-10-09 14:36:34 -0500

    Update sola.gd

--- Player/sola.gd
+++ Player/sola.gd
@@ -1,31 +1,55 @@
 extends Area2D
-# $SolaAnimation.play("walk_down")
+signal hit
 
 @export var speed = 200
 var screen_size
+var beam_speed = 1000
+var beam = preload("res://light_beam.tscn")
+var drift = preload("res://light_drift.tscn")
+var velocity = Vector2(0, 0)
+
+# Movement limits (add as many as you want Team 10):
+var y_max = 400
+
+@onready var head = $Marker2D
 
 func _ready():
+	$AnimatedSprite2D.play("idle")
 	screen_size = get_viewport_rect().size
-	
-
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-		$AnimatedSprite2D.play("walk_right")
+		$Marker2D.position = Vector2(50, 0)	
+		
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-		$AnimatedSprite2D.play("walk_left")
+		$Marker2D.position = Vector2(-50, 0)
+					
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 		$AnimatedSprite2D.play("walk_down")
+		$Marker2D.position = Vector2(0,50)	
+		
+		
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		velocity.y -= 1	
 		$AnimatedSprite2D.play("walk_up")
+		$Marker2D.position = Vector2(0,-50)	
+	
+	if velocity.x != 0:	
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x > 0
 		
-		
+	if Input.is_action_just_released("attack"):
+		speed_shine()
+	
+	if Input.is_action_just_released("attack"):
+		Light_Drift()
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -33,6 +57,54 @@ func _process(delta):
 	else:
 		$AnimatedSprite2D.stop()
 
-		
+	# Implementing and Limiting player movement
 	position += velocity * delta
+	
+	if position.y < y_max:
+		position = Vector2(position.x,y_max)
+	
+	# Limiting movement to within the screen
 	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	
+func _on_body_entered(body):
+	if body is Mob:
+		hide()
+		hit.emit()
+		$CollisionShape2D.set_deferred("disabled",true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func speed_shine():
+	var beam_instance = beam.instantiate()
+#	beam_instance.look_at(get_global_mouse_position())
+	get_parent().add_child(beam_instance)
+	beam_instance.global_position = $Marker2D.global_position
+	beam_instance.velocity = $Marker2D.position
+	if$Marker2D.position == Vector2(50, 0):
+		beam_instance.rotation = 0;
+	elif$Marker2D.position == Vector2(-50, 0):
+		beam_instance.rotation = -110;
+	elif$Marker2D.position == Vector2(0, 50):
+		beam_instance.rotation = -80;
+	elif$Marker2D.position == Vector2(0, -50):
+		beam_instance.rotation = 80;
+
+func Light_Drift():
+	var drift_instance = drift.instantiate()
+	
+	get_parent().add_child(drift_instance)
+	drift_instance.global_position = $Marker2D.global_position
+	drift_instance.velocity = $Marker2D.position
+	if$Marker2D.position == Vector2(50, 0):
+		drift_instance.rotation = 0;
+	elif$Marker2D.position == Vector2(-50, 0):
+		drift_instance.rotation = -110;
+	elif$Marker2D.position == Vector2(0, 50):
+		drift_instance.rotation = -80;
+	elif$Marker2D.position == Vector2(0, -50):
+		drift_instance.rotation = 80;
+		
