0eeba0c8852adac09b6b1d5b5ed31752d5318d59
Kweonsuni <suncham0206@gmail.com>
2023-10-23 11:01:55 -0500

    From title to tutorial
    Able to transition to tutorial through title.

--- title.gd
+++ title.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var timer= 2.5
+var count_down = false
 
+func _ready():
+	$CanvasLayer.hide()
+
+func _on_start_button_pressed():
+	$CanvasLayer.show()
+	$CanvasLayer/AnimationPlayer.play("fade")
+	count_down = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	if count_down==true:
+		timer -= 1 * delta
+	
+	if timer <= 0:
+		get_tree().change_scene_to_file("res://tutorial.tscn")
--- tutorial.gd
+++ tutorial.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
+var timer= 2.5
+var count_down = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	count_down = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	if count_down==true:
+		timer -= 1 * delta
+	
+	if timer <= 0:
+		$CanvasLayer/Fade.modulate.a -= 1*delta
+		$CanvasLayer/Message.modulate.a -= 1*delta
