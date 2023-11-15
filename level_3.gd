28c0cc46529c40feb6acf1484e902c79f6a6ef12
UTDAC <107150263+UTDAC@users.noreply.github.com>
2023-11-15 15:10:57 -0600

    mob3 orientation

--- level_3.gd
+++ level_3.gd
@@ -62,6 +62,7 @@ func _on_mob_timer_timeout():
 	var dir = $Sola.position - mob_spawn_location.position
 	var dir_angle = tan(dir.y/dir.x)
 	
+
 	if num_mob1 < 11: # SPAWNING MOB TYPE 1
 		num_mob1 += 1
 		var mob = preload("res://mob.tscn").instantiate()
@@ -83,7 +84,8 @@ func _on_mob_timer_timeout():
 		var velocity2 = mob2_pos.direction_to($Sola.position) * randf_range(150.0,250.0) 
 		mob2.linear_velocity = velocity2
 		add_child(mob2)
-		
+
+	
 	if num_mob3 < 5 && num_mob1%2 == 0: # SPAWNS MOB TYPE 3
 		num_mob3 += 1
 		print("mob3 spawning")
--- mob3.gd
+++ mob3.gd
@@ -5,60 +5,17 @@ var dead = false
 var player = null
 var player_chase = false
 var speed = 100
-var x = 0.0
+
+var orientation = "right" # way the mob is facing
+var quadrant # where is the mob relative to the player
+
+var dir_vector # vector from mob to sola
+var angle #angle between mob and sola
 
 func _ready():
 	# Beginning mob animation
-	$AnimatedSprite2D.play("move3")
+	$AnimatedSprite2D.play("horizontal")
 	$Squirrel_DeathSprite.visible = false
-	
-func _physics_process(delta):
-	if player_chase:
-		rotation = 0
-		var dir_vector = Vector2((player.position.x-position.x),(player.position.y-position.y))
-		
-		# ENSURING UPRIGHT -> need to implement vertical sprites
-		if dir_vector.x < 0 && scale.x > 0:
-			scale.x *= -1
-		
-			
-		if dir_vector.length() > 30:
-			# MOVEMENT AND DIRECTION CODE
-			linear_velocity = dir_vector/dir_vector.length()*speed
-			#look_at(player.position)
-		else:
-			linear_velocity = Vector2(-dir_vector.x,-dir_vector.y)*speed/2
-	# Checking death condition each loop
-	if dead == true:
-		$AnimatedSprite2D.stop()
-		#$Shadow1_DeathSprite.play("death")
-		death()
-	
-	# ADJUSTING X VALUE FOR FUNCTION
-	x += 0.1
-
-# Any 2D body the mob collides with is identified and processed here
-func _on_body_entered(body):
-	print(body.get_name())
-	if body.get_name() == "FlashLight":
-		#$Death3.play()
-		print("mob3 killed")
-		$AnimatedSprite2D.visible = false
-		#$Squirrel_DeathSprite.visible = true
-		dead = true
-		$CollisionShape2D.set_deferred("disabled",true)
-		
-	if body.is_in_group("monster_bounds"):
-		queue_free()
-		$CollisionShape2D.set_deferred("disabled",true)
-
-
-# Function for eliminating Mob instance
-func death():
-	#if $Squirrel_DeathSprite.frame == 3:
-	hide()
-	#if $Death3.playing == false:
-	queue_free()
 
 
 # Mob detecting sola collision area
@@ -69,6 +26,124 @@ func _on_detection_area_body_entered(body):
 
 func _on_detection_area_body_exited(body):
 	player_chase = false
+
+
+
+
+
+func _physics_process(delta):
+	# Checking death condition each loop
+	if dead == true:
+		$AnimatedSprite2D.stop()
+		#$Shadow1_DeathSprite.play("death")
+		death()
+
+
+	if player_chase:
+		rotation = 0
+		dir_vector = Vector2((player.position.x-position.x),(player.position.y-position.y))
+		angle = atan2(dir_vector.y,dir_vector.x)
+
+
+
+		if abs(dir_vector.x) > abs(dir_vector.y):
+			$AnimatedSprite2D.play("horizontal")
+			if (dir_vector.x) <= 0:
+				scale.x = -1
+			else:
+				scale.x = 1
+
+		else:
+			$AnimatedSprite2D.play("vertical")
+			if (dir_vector.y) <= 0:
+				scale.y = -1
+			else:
+				scale.y = 1
+
+
+
+		if dir_vector.length() > 10:
+			# MOVEMENT AND DIRECTION CODE
+			linear_velocity = dir_vector/dir_vector.length()*speed
+			#look_at(player.position)
+		else:
+			linear_velocity = Vector2(-dir_vector.x,-dir_vector.y)*speed/2
+
+
+
+
+
+
+
+'''
+		# CHECKING IF MOB NEEDS TO REORIENTATE
+		if orientation != "left" && angle <= PI/4 && angle >= -PI/4:
+			$Vertical_Sprite.stop()
+			$Vertical_Sprite.hide()
+			$Horizontal_Sprite.play("h3")
+			$Horizontal_Sprite.show()
+			orientation = "left"
+
+		elif orientation != "right" && angle >= 3*PI/4 && angle < -3*PI/4:
+			$Vertical_Sprite.stop()
+			$Vertical_Sprite.hide()
+			$Horizontal_Sprite.play("h3")
+			$Horizontal_Sprite.show()
+			orientation = "right"
+
+		elif orientation != "down" && angle >= -3*PI/4 && angle < -PI/4:
+			$Vertical_Sprite.play("v3")
+			$Vertical_Sprite.show()
+			$Horizontal_Sprite.stop()
+			$Horizontal_Sprite.hide()
+			orientation = "down"
+
+		elif orientation != "up" && angle < 3*PI/4 && angle > PI/4:
+			$Vertical_Sprite.play("v3")
+			$Vertical_Sprite.show()
+			$Horizontal_Sprite.stop()
+			$Horizontal_Sprite.hide()
+			orientation = "up"
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Any 2D body the mob collides with is identified and processed here
+func _on_body_entered(body):
+	print(body.get_name())
+	if body.get_name() == "FlashLight":
+		#$Death3.play()
+
+		$CollisionShape.visible = false
+		#$Squirrel_DeathSprite.visible = true
+		dead = true
+		$CollisionShape.set_deferred("disabled",true)
+		
+	if body.is_in_group("monster_bounds"):
+		queue_free()
+		$CollisionShape.set_deferred("disabled",true)
+
+
+# Function for eliminating Mob instance
+func death():
+	#if $Squirrel_DeathSprite.frame == 3:
+	hide()
+	#if $Death3.playing == false:
+	queue_free()
+
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
